e5ed1d12a1758ea7ac07e93eaa2e53926bea5e09
SpeedSharma <speedsniper919@gmail.com>
2021-04-30 14:48:21 +0530

    added logo

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,7 +12,7 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'SpeedShop',
+  title: 'THE SKIN ROUTE',
   description: 'We sell the best products for cheap',
   keywords: 'electronics, buy electronics, cheap electroincs',
 }
